f40d1ccfb10b1a96d7188b94c2a7e6b275592262
HARI SAI PARASA <harisaiparasa@gmail.com>
2024-10-09 22:08:29 +0530

    Initial commit of Automated TimeTable Generator project

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from src.controllers.timetable_controller import app as timetable_app
+from src.controllers.timetable_controller import timetable_app  # Import the blueprint
 
 # Initialize Flask app and define static/template folders
 app = Flask(__name__, static_folder='web/static', template_folder='web/templates')
 
-# Register Blueprints or routes from controllers
+# Register the blueprint
 app.register_blueprint(timetable_app)
 
 if __name__ == '__main__':
--- src/controllers/timetable_controller.py
+++ src/controllers/timetable_controller.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template
 from src.services.timetable_service import TimetableService
 import os
 
-template_dir = os.path.abspath('web/templates')
-app = Flask(__name__, template_folder=template_dir)
+# Create a blueprint
+timetable_app = Blueprint('timetable_app', __name__, template_folder=os.path.abspath('web/templates'))
 
-@app.route('/')
+# Routes
+@timetable_app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/generate_timetable', methods=['POST'])
+@timetable_app.route('/generate_timetable', methods=['POST'])
 def generate_timetable():
     data = request.get_json()
     school = data.get('school', {})

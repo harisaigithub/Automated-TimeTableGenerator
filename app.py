fbf9e58f1c2dd0fa1ad2714490b49df8d41ad0c1
HARI SAI PARASA <harisaiparasa@gmail.com>
2024-10-09 21:07:58 +0530

    Initial commit of Automated TimeTable Generator project

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from src.controllers.timetable_controller import app
+from flask import Flask
+from src.controllers.timetable_controller import app as timetable_app
+
+app = Flask(__name__, static_folder='web/static', template_folder='web/templates')
+
+# Registering Blueprint (if you are using Blueprints)
+app.register_blueprint(timetable_app)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
